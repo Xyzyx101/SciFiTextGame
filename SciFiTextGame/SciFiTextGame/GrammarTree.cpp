2454cefab620f741adb7f93551e5c5274d75461a
Andrew Perrault <andrew.perrault@yahoo.ca>
2014-09-26 12:01:16 -0400

    Started grammar tree read algorithm

--- SciFiTextGame/SciFiTextGame/GrammarTree.cpp
+++ SciFiTextGame/SciFiTextGame/GrammarTree.cpp
@@ -1,24 +1,17 @@
 #include "GrammarTree.h"
 #include<vector>
 
-GrammarTree::GrammarTree() :
-current(nullptr),
-currentParent(nullptr) {
+GrammarTree::GrammarTree() {
 	root = std::make_shared<Node>();
 }
 
 GrammarTree::~GrammarTree() {}
 
-void GrammarTree::Reset() {
-	current = root;
-	currentParent = nullptr;
-}
-
 void GrammarTree::AddNode(Token_ptr const token, const std::string& alias) {
-	AddNode(root, token, alias);
+	AddNode_r(root, token, alias);
 }
 
-void GrammarTree::AddNode(Node_ptr node, Token_ptr const token, const std::string& alias) {
+void GrammarTree::AddNode_r(Node_ptr node, Token_ptr const token, const std::string& alias) {
 	std::vector<Edge_ptr>::iterator edgeIter = node->children.begin();
 	std::string partial = "";
 	while( edgeIter != node->children.end() ) {
@@ -31,7 +24,7 @@ void GrammarTree::AddNode(Node_ptr node, Token_ptr const token, const std::strin
 			}
 		}
 		if( partial == edgeString ) {
-			AddNode((*edgeIter)->node, token, alias.substr(partial.length()));
+			AddNode_r((*edgeIter)->node, token, alias.substr(partial.length()));
 			return;
 		} else if( partial != "" ) {
 			break;
@@ -52,9 +45,56 @@ void GrammarTree::AddNode(Node_ptr node, Token_ptr const token, const std::strin
 	}
 }
 
-Token_ptr const GrammarTree::Search(const std::string& string) const {
+std::vector<Token_ptr> GrammarTree::Tokenize(std::istream& inStream) const {
+	std::vector<Token_ptr> tokens;
+	while( inStream.good() ) {
+		Find(root, inStream);
+	}
+	return tokens;
+}
 
-	//FIXME - this should do stuff
-	return nullptr;
+Token_ptr GrammarTree::Find(Node_ptr node, std::istream& inStream) const {
+	int foundElements;
+	std::vector<Edge_ptr>::iterator edgeIter = node->children.begin();
+	do {
+		std::string prefix = (*edgeIter)->prefix;
+		while( inStream.good() &&
+			   foundElements < prefix.length() ) {
+			char element = inStream.get();
+			if( element == prefix[foundElements] ) {
+				++foundElements;
+			} else {
+				// no token found
+			}
+		}
+		++edgeIter;
+	} while( foundElements == 0 &&
+			 edgeIter != node->children.end() )
+	/*
+	// Begin at the root with no elements found
+	Node traverseNode : = root;
+	int elementsFound : = 0;
+
+	// Traverse until a leaf is found or it is not possible to continue
+	while( traverseNode != null && !traverseNode.isLeaf() && elementsFound < x.length ) {
+		// Get the next edge to explore based on the elements not yet found in x
+		Edge nextEdge : = select edge from traverseNode.edges where edge.label is a prefix of x.suffix(elementsFound)
+			// x.suffix(elementsFound) returns the last (x.length - elementsFound) elements of x
+
+			// Was an edge found?
+		if( nextEdge != null ) {
+			// Set the next node to explore
+		traverseNode: = nextEdge.targetNode;
+
+			// Increment elements found based on the label stored at the edge
+			elementsFound += nextEdge.label.length;
+		} else {
+			// Terminate loop
+		traverseNode: = null;
+		}
+	}
+
+	// A match is found if we arrive at a leaf node and have used up exactly x.length elements
+	return (traverseNode != null && traverseNode.isLeaf() && elementsFound == x.length);*/
 
 }
--- SciFiTextGame/SciFiTextGame/GrammarTree.h
+++ SciFiTextGame/SciFiTextGame/GrammarTree.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<string>
+#include<iostream>
 #include"CommonTypes.h"
 #include"GrammarTree_Node.h"
 #include"GrammarTree_Edge.h"
@@ -8,13 +9,24 @@ class GrammarTree {
 public:
 	GrammarTree();
 	~GrammarTree();
-	Token_ptr const Search( const std::string& string ) const;
+
+	/* This is used to initially build the grammar tree. */
 	void AddNode(Token_ptr const token, const std::string& alias);
+
+	Token_ptr const Search( const std::string& string ) const;
+	
+	/* This takes an stream buffer and converts it a list of vector. */
+	std::vector<Token_ptr> Tokenize(std::istream& inStream) const;
+	
 private:
-	void AddNode(Node_ptr node, Token_ptr const token, const std::string& alias);
+	/* This recursively walks the tree and adds the node at the correct place. */
+	void AddNode_r(Node_ptr node, Token_ptr const token, const std::string& alias);
+	
+	/* This will split off the beginning of the stream and return it as a token.
+		The stream will be updated with each call. */
+	Token_ptr Find(Node_ptr node, std::istream& inStream) const;
+
 	void Reset();
 
 	Node_ptr		root;
-	Node_ptr		current;
-	Node_ptr		currentParent;
 };
