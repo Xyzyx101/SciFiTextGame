bb389b7484ab7f655fdd0ff449ace3ebc6166be0
Andrew Perrault <andrew.perrault@yahoo.ca>
2014-10-02 07:23:28 -0400

    Forgot to save one file.  This is part of the last commit

--- SciFiTextGame/SciFiTextGame/Room.h
+++ SciFiTextGame/SciFiTextGame/Room.h
@@ -6,11 +6,11 @@ public:
 	Room(std::string name, GameObject_ptr parent, std::string description, std::string longDescription);
 	virtual ~Room();
 	GameObject_ptr GetExit(std::string name) const;
-	void AddExit(GameObject_ptr newExit);
+	void							AddExit(GameObject_ptr newExit);
 
-	static inline GameObject_t GetType() { return ROOM; };
+	static inline GameObject_t		GetType() { return ROOM; };
 
 private:
-	std::vector<GameObject_ptr> exits;
+	std::vector<GameObject_ptr>		exits;
 };
 
